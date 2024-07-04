a21ce1acb2ff59294c7de7b7d9b28f3e9b4cf448
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-07-04 15:14:22 +0700

    q1

--- q2.ts
+++ q2.ts
@@ -1,9 +1,12 @@
 // define interface for Student object
 /* Your code here */
+function students(x){
+  x = students1.map((std) => std.score);
+}
 
 // assign interface/type to the function definition properly
 function findTopNames(students) {
-  /* Your code here */
+  const y = students.map((std) => std.score > 8);
 }
 
 // assign interface/type to the student1 object properly
